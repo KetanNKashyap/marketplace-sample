c899f01099ecf11342ccecfe29b48dea077c564d
Ketan N Kashyap (GSS Delivery Partner) <ketannk@nousinfo.com>
2018-12-05 21:59:35 +0530

    set up app for heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/db-marketplace', function(error){
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/db-marketplace', function(error){
     if(error) console.log(error);
 
         console.log("connection successful");
